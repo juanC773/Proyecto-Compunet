23eafb7e4b0a4d7c73adad324368e8f5ba34d5b7
juanC773 <juan_cg_g@hotmail.com>
2024-05-14 18:56:59 -0500

    feat: slice2js and slice2java

--- clientHtml/index.js
+++ clientHtml/index.js
@@ -1,6 +1,9 @@
 const communicator = Ice.initialize();
 
 
+
+
+
 async function initProcess() {
     const hostname = document.location.hostname || "127.0.0.1";
     const proxy = communicator.stringToProxy(`chat:ws -h ${hostname} -p 10000`);
