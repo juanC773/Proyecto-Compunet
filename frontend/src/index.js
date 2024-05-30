87b9e510534edd0ebe31e45435e6ee4bf3480804
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-05-29 23:46:10 -0500

    implement routes for frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,34 @@
 import ProductsView from "./layout/ProductsView.js";
 import CartView from "./layout/CartView.js";
 import AddProductView from "./layout/AddProductView.js";
+import MainView from "./layout/MainView.js";
 
 async function displayView(view) {
     let res = await view();
     const $root = document.querySelector('#root');
+    $root.innerHTML = '';
     $root.appendChild(res);
 }
 
-displayView(ProductsView)
-displayView(CartView)
-displayView(AddProductView)
+const setRoute = () => {
+    console.log(window.location.hash)
+    switch (window.location.hash) {
+        case '#home':
+            displayView(MainView);
+            break;
+        case '#products':
+            displayView(ProductsView);
+            break;
+        case '#cart':
+            displayView(CartView);
+            break;
+        case '#add-product':
+            displayView(AddProductView);
+            break;
+        default:
+            displayView(MainView);
+    }
+}
+
+window.addEventListener('load', setRoute);
+window.addEventListener('hashchange', setRoute);
---
+++ frontend/src/layout/MainView.js
@@ -0,0 +1,39 @@
+export default async function MainView() {
+    const container = document.createElement('div');
+    container.id = 'fh5co-page';    
+    const divWork = document.createElement('div');
+    divWork.id = 'fh5co-work';
+
+    const divContainer = document.createElement('div');
+    divContainer.className = 'container';
+
+    const divRow = document.createElement('div');
+    divRow.className = 'row top-line animate-box';
+
+    const divCol = document.createElement('div');
+    divCol.className = 'col-md-7 col-md-push-5 text-left title-container';
+
+    const h2 = document.createElement('h2');
+    h2.className = 'main-title';
+    h2.innerHTML = 'Bienvenido a la mejor tienda!<br> ten un buen dia (:';
+
+    const divSubtext = document.createElement('div');
+    divSubtext.className = 'subtext';
+
+    const spanHighlight = document.createElement('span');
+    spanHighlight.className = 'fh5co-highlight';
+    spanHighlight.innerHTML = 'Productos con <span id="corazon" class="heart">&#10084;</span> <span class="gradient-text">?</span>';
+
+    // Agregar los elementos al DOM
+    divSubtext.appendChild(spanHighlight);
+    divCol.appendChild(h2);
+    divCol.appendChild(divSubtext);
+    divRow.appendChild(divCol);
+    divContainer.appendChild(divRow);
+    divWork.appendChild(divContainer);
+
+    // Agregar el div principal al elemento root
+    const root = document.getElementById('root');
+    container.appendChild(divWork);
+    return container;
+}
